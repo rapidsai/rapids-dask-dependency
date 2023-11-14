91d6bb4a1e3551ed92a92ef6fb28be11c65d6148
Ray Douglass <3107146+raydouglass@users.noreply.github.com>
2023-11-14 17:23:26 -0500

    Add update-version.sh (#5)
    Adds `ci/release/update-version.sh` to update the RAPIDS versions when a
    new branch is created.
    
    I thought the version in `pyproject.toml` should be PEP-440 style, but
    it wasn't originally, so I didn't change that.
    
    ---------
    
    Co-authored-by: Vyas Ramasubramani <vyas.ramasubramani@gmail.com>

---
+++ ci/release/update-version.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# Copyright (c) 2023, NVIDIA CORPORATION.
+
+## Usage
+# bash update-version.sh <new_version>
+
+# Format is YY.MM.PP - no leading 'v' or trailing 'a'
+NEXT_FULL_TAG=$1
+
+# Get current version
+CURRENT_TAG=$(git tag --merged HEAD | grep -xE '^v.*' | sort --version-sort | tail -n 1 | tr -d 'v')
+
+#Get <major>.<minor> for next version
+NEXT_MAJOR=$(echo "$NEXT_FULL_TAG" | awk '{split($0, a, "."); print a[1]}')
+NEXT_MINOR=$(echo "$NEXT_FULL_TAG" | awk '{split($0, a, "."); print a[2]}')
+NEXT_PATCH=$(echo "$NEXT_FULL_TAG" | awk '{split($0, a, "."); print a[3]}')
+NEXT_SHORT_TAG=${NEXT_MAJOR}.${NEXT_MINOR}
+NEXT_FULL_TAG=${NEXT_MAJOR}.${NEXT_MINOR}.${NEXT_PATCH}
+
+echo "Preparing release $CURRENT_TAG => $NEXT_FULL_TAG"
+
+# Inplace sed replace; workaround for Linux and Mac
+function sed_runner() {
+  sed -i.bak ''"$1"'' $2 && rm -f ${2}.bak
+}
+
+sed_runner "s/^version = .*/version = \"${NEXT_FULL_TAG}a0\"/" pip/rapids_dask_dependency/pyproject.toml
+
+for FILE in .github/workflows/*.yaml; do
+  sed_runner "/shared-workflows/ s/@.*/@branch-${NEXT_SHORT_TAG}/g" "${FILE}"
+done

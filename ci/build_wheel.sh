c48b28aa7793cc6d44206c48b86f38444e72a411
Vyas Ramasubramani <vyasr@nvidia.com>
2024-02-27 01:21:56 +0000

    Update wheel-building CI script
    Signed-off-by: Vyas Ramasubramani <vyasr@nvidia.com>

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-# Copyright (c) 2023, NVIDIA CORPORATION.
+# Copyright (c) 2023-2024, NVIDIA CORPORATION.
 
 set -euo pipefail
 
 source rapids-configure-sccache
 source rapids-date-string
 
-package_name=rapids-dask-dependency
-package_dir="pip/${package_name}"
 version=$(rapids-generate-version)
 
-sed -i "s/^version = .*/version = \"${version}\"/g" "${package_dir}/pyproject.toml"
+sed -i "s/^version = .*/version = \"${version}\"/g" "pyproject.toml"
 
-cd "${package_dir}"
 python -m pip wheel . -w dist -vvv --no-deps --disable-pip-version-check
 
-RAPIDS_PY_WHEEL_NAME="${package_name}" rapids-upload-wheels-to-s3 dist
+RAPIDS_PY_WHEEL_NAME="rapids-dask-dependency" rapids-upload-wheels-to-s3 dist

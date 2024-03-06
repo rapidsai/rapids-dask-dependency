99d4fe55ec03897e526e027a5e532382578a2a2b
Vyas Ramasubramani <vyasr@nvidia.com>
2024-03-06 17:59:18 +0000

    Install package for conda build

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -10,6 +10,6 @@ version=$(rapids-generate-version)
 
 sed -i "s/^version = .*/version = \"${version}\"/g" "pyproject.toml"
 
-python -m pip wheel . -w dist -vvv --no-deps --disable-pip-version-check
+python -m pip wheel . -w dist -vv --no-deps --disable-pip-version-check
 
 RAPIDS_PY_WHEEL_NAME="rapids-dask-dependency" rapids-upload-wheels-to-s3 dist

bb83337f444b2a911f1b8cc03bdef118c49bbda6
Vyas Ramasubramani <vyasr@nvidia.com>
2024-03-06 21:53:57 +0000

    Make conda add test files to test tree

--- conda/recipes/rapids-dask-dependency/run_test.sh
+++ conda/recipes/rapids-dask-dependency/run_test.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 # Copyright (c) 2024, NVIDIA CORPORATION.
 
-echo "The recipe dir is: ${RECIPE_DIR}"
-echo "The source dir is: ${SRC_DIR}"
-cd ${RECIPE_DIR}/../../..
-ls
 python -m pytest -v tests/

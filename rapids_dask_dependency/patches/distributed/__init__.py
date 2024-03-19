54929adc68feda161a41e89004bc76cefef39f86
Vyas Ramasubramani <vyasr@nvidia.com>
2024-03-19 06:55:27 -0700

    Enable patching (#27)
    This PR adds the ability to monkey-patch imports of dask and distributed
    whenever those imports occur by simply installing
    rapids-dask-dependency.
    
    There's a tiny bit of scope creep here because this PR added real Python
    code to the repo for the first time, so I also added pre-commit hooks
    that in turn modified some unrelated files (only minimally, though).
    
    TODO:
    - [x] Update conda CI and packaging
    - [ ] Stress test extensively
    
    ---------
    
    Signed-off-by: Vyas Ramasubramani <vyasr@nvidia.com>
    Co-authored-by: Richard (Rick) Zamora <rzamora217@gmail.com>

---
+++ rapids_dask_dependency/patches/distributed/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) 2024, NVIDIA CORPORATION.
+
+from .add_patch_attr import add_patch_attr
+
+patches = [add_patch_attr]
---
+++ tests/test_patch.py
@@ -0,0 +1,26 @@
+from functools import wraps
+from multiprocessing import Process
+
+
+def run_test_in_subprocess(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        p = Process(target=func, args=args, kwargs=kwargs)
+        p.start()
+        p.join()
+
+    return wrapper
+
+
+@run_test_in_subprocess
+def test_dask():
+    import dask
+
+    assert hasattr(dask, "_rapids_patched")
+
+
+@run_test_in_subprocess
+def test_distributed():
+    import distributed
+
+    assert hasattr(distributed, "_rapids_patched")

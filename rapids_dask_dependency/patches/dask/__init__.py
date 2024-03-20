253c4288bd22c4bf1eb08f1eecd694781e20f78a
rjzamora <rzamora217@gmail.com>
2024-03-20 10:23:11 -0700

    override the query-planning default

--- rapids_dask_dependency/patches/dask/__init__.py
+++ rapids_dask_dependency/patches/dask/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) 2024, NVIDIA CORPORATION.
 
 from .add_patch_attr import add_patch_attr
+from .config_defaults import set_config_defaults
 
-patches = [add_patch_attr]
+patches = [add_patch_attr, set_config_defaults]

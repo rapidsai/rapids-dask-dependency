b3c2e9a6f1c036d60c9bbf39377bc404a40740e5
Vyas Ramasubramani <vyasr@nvidia.com>
2024-04-04 01:14:47 +0000

    Add comment indicating what we're patching

--- rapids_dask_dependency/patches/dask/dataframe/accessor.py
+++ rapids_dask_dependency/patches/dask/dataframe/accessor.py
@@ -2,5 +2,6 @@
 
 from rapids_dask_dependency.importer import VendoredImporter
 
+# Currently vendoring this module due to https://github.com/dask/dask/pull/11035
 _importer = VendoredImporter(__name__)
 load_module = _importer.load_module

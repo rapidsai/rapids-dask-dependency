505dc479cdc57c3746abf0db64e416997ee300cb
Vyas Ramasubramani <vyasr@nvidia.com>
2024-04-04 16:26:35 +0000

    Only patch accessor when needed

--- rapids_dask_dependency/patches/dask/dataframe/accessor.py
+++ rapids_dask_dependency/patches/dask/dataframe/accessor.py
@@ -1,7 +1,13 @@
 # Copyright (c) 2024, NVIDIA CORPORATION.
+import sys
 
-from rapids_dask_dependency.importer import VendoredImporter
+if sys.version_info >= (3, 11, 9):
+    from dask import __version__
+    from packaging.version import Version
 
-# Currently vendoring this module due to https://github.com/dask/dask/pull/11035
-_importer = VendoredImporter(__name__)
-load_module = _importer.load_module
+    if Version(__version__) < Version("2024.4.1"):
+        from rapids_dask_dependency.importer import VendoredImporter
+
+        # Currently vendoring this module due to https://github.com/dask/dask/pull/11035
+        _importer = VendoredImporter(__name__)
+        load_module = _importer.load_module

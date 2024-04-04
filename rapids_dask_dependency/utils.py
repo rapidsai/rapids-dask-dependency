dec28b0d6b53457738aa63c0b8e1b6ed15c4b93e
Vyas Ramasubramani <vyasr@nvidia.com>
2024-04-04 12:46:20 -0700

    Generalize patching to support vendoring (#39)
    This PR makes a number of significant changes to the patching infrastructure:
    1. This PR reorganizes the patching logic to be based on a more principled approach. Rather than maintaining lists of patch functions that are each responsible for filtering modules to apply themselves to, patches are organized in the patches directory in a tree structure matching dask itself. Patches are found and run by importing the same relative paths within the `patches` directory corresponding to a particular dask or distributed module.
    2. It adds proper support for patching submodules. Previously the loader was being disabled whenever a real dask module was being imported, but this is problematic because if some dask modules import others they will pre-populate `sys.modules` with the real modules and therefore the loader will never be used for loading a patched version of the submodule.
    3. Patches are no longer just functions applied to modules, they are arbitrary functions executed when a module is imported. As a result, a wider range of modifications is possible than was previously allowed. In particular:
    4. The more general functions allow the entire module import to be hijacked and redirected to other modules.
    5. The new framework is used to vendor a patched version of the accessor.py module in dask, which resolves the issues observed in https://github.com/dask/dask/pull/11035.
    
    Authors:
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    Approvers:
      - Richard (Rick) Zamora (https://github.com/rjzamora)
    
    URL: https://github.com/rapidsai/rapids-dask-dependency/pull/39

---
+++ rapids_dask_dependency/utils.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2024, NVIDIA CORPORATION.
+
+import warnings
+from contextlib import contextmanager
+from functools import lru_cache
+
+original_warn = warnings.warn
+
+
+@lru_cache
+def _make_warning_func(level):
+    def _warning_with_increased_stacklevel(
+        message, category=None, stacklevel=1, source=None, **kwargs
+    ):
+        # Patch warnings to have the right stacklevel
+        original_warn(message, category, stacklevel + level, source, **kwargs)
+
+    return _warning_with_increased_stacklevel
+
+
+@contextmanager
+def patch_warning_stacklevel(level):
+    previous_warn = warnings.warn
+    warnings.warn = _make_warning_func(level)
+    yield
+    warnings.warn = previous_warn
+
+
+# Note: The Python documentation does not make it clear whether we're guaranteed that
+# spec is not a copy of the original spec, but that is the case for now. We need to
+# assign this because the spec is used to update module attributes after it is
+# initialized by create_module.
+def update_spec(spec, original_spec):
+    spec.origin = original_spec.origin
+    spec.submodule_search_locations = original_spec.submodule_search_locations
+    return spec

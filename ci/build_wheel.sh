7b8fae55bce7a2feeb2ee9e4dd506334b1aea6c0
VenkateshJaya <jjayabaskar@nvidia.com>
2025-04-03 16:18:50 -0700

    removed wheel_dir variable from ci scripts

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -6,16 +6,14 @@ set -euo pipefail
 source rapids-configure-sccache
 source rapids-date-string
 
-wheel_dir=${RAPIDS_WHEEL_BLD_OUTPUT_DIR}
-
 version=$(rapids-generate-version)
 
 sed -i "s/^version = .*/version = \"${version}\"/g" "pyproject.toml"
 
-python -m pip wheel . -w "${wheel_dir}" -vv --no-deps --disable-pip-version-check
+python -m pip wheel . -w "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}" -vv --no-deps --disable-pip-version-check
 
-RAPIDS_PY_WHEEL_NAME="rapids-dask-dependency" RAPIDS_PY_WHEEL_PURE="1" rapids-upload-wheels-to-s3 "${wheel_dir}"
+RAPIDS_PY_WHEEL_NAME="rapids-dask-dependency" RAPIDS_PY_WHEEL_PURE="1" rapids-upload-wheels-to-s3 python "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
 
 # Run tests
-python -m pip install "$(echo ${wheel_dir}/*.whl)[test]"
+python -m pip install "$(echo ${RAPIDS_WHEEL_BLD_OUTPUT_DIR}/*.whl)[test]"
 python -m pytest -v tests/

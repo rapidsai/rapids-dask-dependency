f7b1ecd99b978dc9f7915092468636b205c106bc
Jaya Venkatesh <jjayabaskar@nvidia.com>
2025-04-11 15:18:41 -0700

    Moving wheel builds to specified location and uploading build artifacts to Github (#92)
    This work is towards moving build artifacts from `downloads.rapids.ai` to Github Artifact Store (see https://github.com/rapidsai/ops/issues/2982)
    
    As part of these changes, all wheel builds need to be in a specified temporary directory indicated by the environment variable `RAPIDS_WHEEL_BLD_OUTPUT_DIR` set on the `ci-wheel` Docker image used for building wheels. This lets us upload all wheel artifacts seamlessly to Github Artifacts.
    
    Authors:
      - Jaya Venkatesh (https://github.com/VenkateshJaya)
    
    Approvers:
      - AJ Schmidt (https://github.com/ajschmidt8)
      - Tom Augspurger (https://github.com/TomAugspurger)
    
    URL: https://github.com/rapidsai/rapids-dask-dependency/pull/92

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -10,10 +10,10 @@ version=$(rapids-generate-version)
 
 sed -i "s/^version = .*/version = \"${version}\"/g" "pyproject.toml"
 
-python -m pip wheel . -w dist -vv --no-deps --disable-pip-version-check
+python -m pip wheel . -w "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}" -vv --no-deps --disable-pip-version-check
 
-RAPIDS_PY_WHEEL_NAME="rapids-dask-dependency" RAPIDS_PY_WHEEL_PURE="1" rapids-upload-wheels-to-s3 dist
+RAPIDS_PY_WHEEL_NAME="rapids-dask-dependency" RAPIDS_PY_WHEEL_PURE="1" rapids-upload-wheels-to-s3 python "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
 
 # Run tests
-python -m pip install "$(echo dist/*.whl)[test]"
+python -m pip install "$(echo ${RAPIDS_WHEEL_BLD_OUTPUT_DIR}/*.whl)[test]"
 python -m pytest -v tests/

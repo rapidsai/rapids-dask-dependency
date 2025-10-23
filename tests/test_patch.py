68a13524e09065ddc82752a7ec42d0221a8b9f53
Tom Augspurger <toaugspurger@nvidia.com>
2025-10-23 10:15:37 -0500

    Python 3.14 compatibility for test_patch (#132)
    This fixes an issue in our test suite somehow not being compatible with Python 3.14. See https://github.com/rapidsai/rapids-dask-dependency/issues/130. The job at
    https://github.com/rapidsai/rapids-dask-dependency/actions/runs/18656091529/job/53185462705#step:10:815 was the first to run with 3.14 and it failed.
    
    (We don't expect to be running with 3.14 there, but that's a [separate issue](https://github.com/rapidsai/rapids-dask-dependency/issues/131))
    
     I haven't confirmed this, but skimming the changelog shows some changes to `multiprocessing` to use `forkserver` instead of `fork`, which has some implications for pickling: https://docs.python.org/3/whatsnew/3.14.html#multiprocessing. To make our test code compatible with pickle, I've
    
    1. Moved the `redirect_stdout_stderr` closure to a top-level function. It wasn't using anything from the closure's namespace so no changes there
    2. Changed our usage of `run_test_in_subprocess` from a decorator to a regular function call on new top-level `check_*` functions, which perform the actual check in the subprocess.
    
    
    Closes https://github.com/rapidsai/rapids-dask-dependency/issues/130
    
    Authors:
      - Tom Augspurger (https://github.com/TomAugspurger)
    
    Approvers:
      - Peter Andreas Entschev (https://github.com/pentschev)
    
    URL: https://github.com/rapidsai/rapids-dask-dependency/pull/132

--- tests/test_patch.py
+++ tests/test_patch.py
@@ -2,62 +2,57 @@
 
 import contextlib
 import subprocess
-import tempfile
-from functools import wraps
 from multiprocessing import Process
 
-import pytest
 
-
-def run_test_in_subprocess(func):
-    def redirect_stdout_stderr(func, stdout, stderr, *args, **kwargs):
-        with open(stdout, "w") as stdout_file, open(stderr, "w") as stderr_file:
-            with (
-                contextlib.redirect_stdout(stdout_file),
-                contextlib.redirect_stderr(stderr_file),
-            ):
-                func(*args, **kwargs)
-
-    @wraps(func)
-    def wrapper(*args, **kwargs):
+def redirect_stdout_stderr(func, stdout, stderr, *args, **kwargs):
+    with open(stdout, "w") as stdout_file, open(stderr, "w") as stderr_file:
         with (
-            tempfile.NamedTemporaryFile(mode="w+") as stdout,
-            tempfile.NamedTemporaryFile(mode="w+") as stderr,
+            contextlib.redirect_stdout(stdout_file),
+            contextlib.redirect_stderr(stderr_file),
         ):
-            p = Process(
-                target=redirect_stdout_stderr,
-                args=(func, stdout.name, stderr.name, *args),
-                kwargs=kwargs,
-            )
-            p.start()
-            p.join()
-            stdout_log = stdout.file.read()
-            stderr_log = stderr.file.read()
-        if p.exitcode != 0:
-            msg = f"Process exited {p.exitcode}."
-            if stdout_log:
-                msg += f"\nstdout:\n{stdout_log}"
-            if stderr_log:
-                msg += f"\nstderr:\n{stderr_log}"
-            raise RuntimeError(msg)
-
-    return wrapper
+            func(*args, **kwargs)
 
 
-@pytest.mark.xfail(reason="Temporary xfail to unblock CI")
-@run_test_in_subprocess
-def test_dask():
+def run_test_in_subprocess(func, tmp_path, *args, **kwargs):
+    stdout = tmp_path / "stdout.log"
+    stderr = tmp_path / "stderr.log"
+    p = Process(
+        target=redirect_stdout_stderr,
+        args=(func, stdout, stderr, *args),
+        kwargs=kwargs,
+    )
+    p.start()
+    p.join()
+    stdout_log = stdout.read_text()
+    stderr_log = stderr.read_text()
+    if p.exitcode != 0:
+        msg = f"Process exited {p.exitcode}."
+        if stdout_log:
+            msg += f"\nstdout:\n{stdout_log}"
+        if stderr_log:
+            msg += f"\nstderr:\n{stderr_log}"
+        raise RuntimeError(msg)
+
+
+def check_dask():
     import dask
 
     assert hasattr(dask, "_rapids_patched")
 
 
-@pytest.mark.xfail(reason="Temporary xfail to unblock CI")
-@run_test_in_subprocess
-def test_distributed():
+def test_dask(tmp_path):
+    run_test_in_subprocess(check_dask, tmp_path)
+
+
+def check_distributed():
     import distributed
 
     assert hasattr(distributed, "_rapids_patched")
+
+
+def test_distributed(tmp_path):
+    run_test_in_subprocess(check_distributed, tmp_path)
 
 
 def test_dask_cli():
